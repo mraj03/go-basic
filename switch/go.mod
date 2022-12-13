db7c7fa120c59d8ba92081cee5c016496bfe7d38
mraj03 <melvinraj0004@gmail.com>
2022-12-13 19:35:43 +0530

    - Created switch folder - Created mod file, main.go file with an switch example code.

---
+++ switch/go.mod
@@ -0,0 +1,3 @@
+module github.com/mraj03/switch-basics
+
+go 1.19
---
+++ switch/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	// let's define the months by the number they type. 
+
+	var month int
+	fmt.Println("Enter the number of the month you were born in: ")
+	fmt.Scanln(&month)
+
+	switch month{
+	case 1:
+		fmt.Println("It is January")
+	
+	case 2:
+		fmt.Println("It is February")
+	
+	case 3:
+		fmt.Println("It is March")
+
+	case 4:
+		fmt.Println("It is April")
+
+	case 5:
+		fmt.Println("It is May")
+
+	case 6:
+		fmt.Println("It is June")
+
+	case 7:
+		fmt.Println("It is July")
+
+	case 8:
+		fmt.Println("It is August")
+
+	case 9:
+		fmt.Println("It is September")
+
+	case 10:
+		fmt.Println("It is October")
+
+	case 11:
+		fmt.Println("It is November")
+
+	case 12:
+		fmt.Println("It is December")
+	}
+	
+	
+}
